93157e080e34c38893c40adf23e772f7444d0076
Lynx <lynx@ilynxcat.me>
2026-03-12 08:09:39 -0700

    add logs when skipping a step

--- update.zsh
+++ update.zsh
@@ -5,19 +5,33 @@ set -euo pipefail
 # (if we somehow catch a false positive the installer will handle it just fine;
 #  completely acceptable failure mode)
 if ! command -v brew &>/dev/null; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo 'OK: Homebrew already installed'
 fi
 
 # Ensure we have stow so we can symlink our dotfiles
 if ! command -v stow &>/dev/null; then
-  brew install stow
+    brew install stow
+else
+    echo 'OK: stow already installed'
 fi
 
 # Clone dotfiles if not present
 PROFILE_DIR="$HOME/.profile"
 if [[ ! -d "$PROFILE_DIR/.git" ]]; then
-  git clone https://github.com/iLynxcat/dotmac "$PROFILE_DIR"
+    git clone https://github.com/iLynxcat/dotmac "$PROFILE_DIR"
+else
+    echo 'OK: ~/.profile already cloned'
 fi
 
 # Make our symlinks
-stow --no-folding -d "$HOME/.profile" .
+stow --no-folding -R -d "$PROFILE_DIR" .
+echo 'OK: stowed files from profile'
+
+# Install casks and formulae from Brewfile
+if ! brew bundle check --file="$PROFILE_DIR/.homebrew/Brewfile" &>/dev/null; then
+    brew bundle install --file="$PROFILE_DIR/.homebrew/Brewfile"
+else
+    echo 'OK: brew bundle already satisfied'
+fi
