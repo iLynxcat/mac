d67fa2af91b6e690f85284cb819fef5692d5e882
Lynx <lynx@ilynxcat.me>
2026-06-08 09:09:09 -0500

    move to new github repo

--- update.zsh
+++ update.zsh
@@ -17,7 +17,7 @@ fi
 # Clone dotfiles if not present
 PROFILE_DIR="$HOME/.profile"
 if [[ ! -d "$PROFILE_DIR/.git" ]]; then
-    git clone https://github.com/iLynxcat/dotmac "$PROFILE_DIR"
+    git clone https://github.com/iLynxcat/mac "$PROFILE_DIR"
 fi
 
 # Needed for local git daemon
